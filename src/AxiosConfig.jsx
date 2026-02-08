751f26b57c15f3333f6be5bb2965ccdc445fe69f
WabiDagim <wabidagim280@gmail.com>
2026-02-08 00:14:41 -1000

    for render

--- src/AxiosConfig.jsx
+++ src/AxiosConfig.jsx
@@ -1,5 +1,6 @@
 import axios from'axios';
 const axiosBase = axios.create({
-    baseURL:'http://localhost:5400/api'
+    // deployed on render
+    baseURL:'https://campus-api-deploy.onrender.com'
 })
 export default axiosBase;
