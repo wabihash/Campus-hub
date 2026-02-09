2f8dc3247cefdcafe279be0e8b56ea537f3625e2
WabiDagim <wabidagim280@gmail.com>
2026-02-08 21:25:40 -1000

    aditf

--- src/AxiosConfig.jsx
+++ src/AxiosConfig.jsx
@@ -1,6 +1,6 @@
 import axios from'axios';
 const axiosBase = axios.create({
     // deployed on render
-    baseURL:'https://campus-api-deploy.onrender.com'
+    baseURL:'https://campus-forum.onrender.com'
 })
 export default axiosBase;
